102961dd036cc8ed107c8b6eb82905133f558547
Bohdan Vakh <bohdanvah@gmail.com>
2023-10-16 20:20:52 +0300

    feat/add controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,8 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @user = current_user
+    @categories = Category.all
+    @user_spendings = Spending.includes(:category).where(user_id: @user.id)
   end
 end
