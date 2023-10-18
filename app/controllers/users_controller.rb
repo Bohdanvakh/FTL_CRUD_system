dd045ab62cec8f3950c2df6b7bcd04585bd645a3
Bohdan Vakh <bohdanvah@gmail.com>
2023-10-18 15:50:32 +0300

    feat/add scopes to index methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,14 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
 
+  has_scope :by_category
+  has_scope :by_amount, using: %i[min max], type: :hash
+
+
   def index
     @user = current_user
     @categories = Category.all
-    @user_spendings = Spending.includes(:category).where(user_id: @user.id)
+
+    @user_spendings = apply_scopes(Spending).includes(:category).all
   end
 end
